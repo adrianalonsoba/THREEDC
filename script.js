867de7090d6db51029374eade8f8dea6ef8f0105
adrianalonsoba <adrian.alonsoba@gmail.com>
2015-07-09 12:03:01 +0200

    añadido mouse over,se muestran los valores de commits, autores y la correspondiente fecha

--- script.js
+++ script.js
@@ -12,6 +12,8 @@ var clock = new THREE.Clock();
 
 // custom global variables
 var projector, mouse = { x: 0, y: 0 }, INTERSECTED;
+var sprite1;
+var canvas1, context1, texture1;
 
 
 
@@ -125,7 +127,7 @@ function init () {
    //  a collection of points ("geometry") and
    //  a set of surface parameters ("material")
 
-   //COMMITS
+   		//COMMITS
    var z=1;
    var y=0;
    var x=1;
@@ -138,13 +140,14 @@ function init () {
 			var material = new THREE.MeshLambertMaterial( {color: "#0000ff"} );
 			var cube = new THREE.Mesh(geometry, material);
 			cube.position.set(x, y, z);
+			cube.name = "Commits:"+json_data.commits[i]+" - "+json_data.date[i];
 			scene.add(cube);
 			x+=1;
 	   };
 	   x=0;
 	   z+=1;
    }
-   //AUTHORS
+ 	 	 //AUTHORS
    var z=20;
    var y=0;
    var x=1;
@@ -157,12 +160,45 @@ function init () {
 			var material = new THREE.MeshLambertMaterial( {color: "#ff0000"} );
 			var cube = new THREE.Mesh(geometry, material);
 			cube.position.set(x, y, z);
+			cube.name = "Authors:"+json_data.authors[i]+" - "+json_data.date[i];
+
 			scene.add(cube);
 			x+=1;
 	   };
 	   x=0;
 	   z+=1;
    }
+
+	// initialize object to perform world/screen calculations
+	projector = new THREE.Projector();
+
+	// when the mouse moves, call the given function
+	document.addEventListener( 'mousemove', onDocumentMouseMove, false );
+
+	/////// draw text on canvas /////////
+
+	// create a canvas element
+	canvas1 = document.createElement('canvas');
+	context1 = canvas1.getContext('2d');
+	context1.font = "Bold 20px Arial";
+	context1.fillStyle = "rgba(0,0,0,0.95)";
+    context1.fillText('Hello, world!', 0, 20);
+    
+	// canvas contents will be used for a texture
+	texture1 = new THREE.Texture(canvas1) 
+	texture1.needsUpdate = true;
+	
+	////////////////////////////////////////
+
+	
+	var spriteMaterial = new THREE.SpriteMaterial( { map: texture1, useScreenCoordinates: true, alignment: THREE.SpriteAlignment.topLeft } );
+	
+	sprite1 = new THREE.Sprite( spriteMaterial );
+	sprite1.scale.set(200,100,1.0);
+	sprite1.position.set( 50, 50, 0 );
+	scene.add( sprite1 );	
+
+	//////////////////////////////////////////
 
 
 }
@@ -173,6 +209,21 @@ function get_random_color() {
   }
   return "#"+c()+c()+c();
 }
+
+function onDocumentMouseMove( event ) 
+{
+	// the following line would stop any other event handler from firing
+	// (such as the mouse's TrackballControls)
+	// event.preventDefault();
+
+	// update sprite position
+	sprite1.position.set( event.clientX, event.clientY - 20, 0 );
+	
+	// update the mouse variable
+	mouse.x = ( event.clientX / window.innerWidth ) * 2 - 1;
+	mouse.y = - ( event.clientY / window.innerHeight ) * 2 + 1;
+}
+
 
 function animate() 
 {
@@ -189,81 +240,73 @@ function render()
 
 function update()
 {
-   // delta = change in time since last call (in seconds)
-   var delta = clock.getDelta(); 
-   controls.update();
+	
+	// create a Ray with origin at the mouse position
+	//   and direction into the scene (camera direction)
+	var vector = new THREE.Vector3( mouse.x, mouse.y, 1 );
+	projector.unprojectVector( vector, camera );
+	var ray = new THREE.Raycaster( camera.position, vector.sub( camera.position ).normalize() );
+
+	// create an array containing all objects in the scene with which the ray intersects
+	var intersects = ray.intersectObjects( scene.children );
+
+	// INTERSECTED = the object in the scene currently closest to the camera 
+	//		and intersected by the Ray projected from the mouse position 	
+	
+	// if there is one (or more) intersections
+	if ( intersects.length > 0 )
+	{
+		// if the closest object intersected is not the currently stored intersection object
+		if ( intersects[ 0 ].object != INTERSECTED ) 
+		{
+		    // restore previous intersection object (if it exists) to its original color
+			if ( INTERSECTED ) 
+				INTERSECTED.material.color.setHex( INTERSECTED.currentHex );
+			// store reference to closest object as current intersection object
+			INTERSECTED = intersects[ 0 ].object;
+			// store color of closest object (for later restoration)
+			INTERSECTED.currentHex = INTERSECTED.material.color.getHex();
+			// set a new color for closest object
+			INTERSECTED.material.color.setHex( 0xffff00 );
+			
+			// update text, if it has a "name" field.
+			if ( intersects[ 0 ].object.name )
+			{
+			    context1.clearRect(0,0,640,480);
+				var message = intersects[ 0 ].object.name;
+				var metrics = context1.measureText(message);
+				var width = metrics.width;
+				context1.fillStyle = "rgba(0,0,0,0.95)"; // black border
+				context1.fillRect( 0,0, width+8,20+8);
+				context1.fillStyle = "rgba(255,255,255,0.95)"; // white filler
+				context1.fillRect( 2,2, width+4,20+4 );
+				context1.fillStyle = "rgba(0,0,0,1)"; // text color
+				context1.fillText( message, 4,20 );
+				texture1.needsUpdate = true;
+			}
+			else
+			{
+				context1.clearRect(0,0,300,300);
+				texture1.needsUpdate = true;
+			}
+		}
+	} 
+	else // there are no intersections
+	{
+		// restore previous intersection object (if it exists) to its original color
+		if ( INTERSECTED ) 
+			INTERSECTED.material.color.setHex( INTERSECTED.currentHex );
+		// remove previous intersection object reference
+		//     by setting current intersection object to "nothing"
+		INTERSECTED = null;
+		context1.clearRect(0,0,300,300);
+		texture1.needsUpdate = true;
+	}
+
+	
+	controls.update();
+	//stats.update();
 }
 
-function makeTextSprite( message, parameters )
-{
-   if ( parameters === undefined ) parameters = {};
-   
-   var fontface = parameters.hasOwnProperty("fontface") ? 
-      parameters["fontface"] : "Arial";
-   
-   var fontsize = parameters.hasOwnProperty("fontsize") ? 
-      parameters["fontsize"] : 18;
-   
-   var borderThickness = parameters.hasOwnProperty("borderThickness") ? 
-      parameters["borderThickness"] : 4;
-   
-   var borderColor = parameters.hasOwnProperty("borderColor") ?
-      parameters["borderColor"] : { r:0, g:0, b:0, a:1.0 };
-   
-   var backgroundColor = parameters.hasOwnProperty("backgroundColor") ?
-      parameters["backgroundColor"] : { r:255, g:255, b:255, a:1.0 };
 
-   var spriteAlignment = THREE.SpriteAlignment.topLeft;
-      
-   var canvas = document.createElement('canvas');
-   var context = canvas.getContext('2d');
-   context.font = "Bold " + fontsize + "px " + fontface;
-    
-   // get size data (height depends only on font size)
-   var metrics = context.measureText( message );
-   var textWidth = metrics.width;
-   
-   // background color
-   context.fillStyle   = "rgba(" + backgroundColor.r + "," + backgroundColor.g + ","
-                          + backgroundColor.b + "," + backgroundColor.a + ")";
-   // border color
-   context.strokeStyle = "rgba(" + borderColor.r + "," + borderColor.g + ","
-                          + borderColor.b + "," + borderColor.a + ")";
 
-   context.lineWidth = borderThickness;
-   roundRect(context, borderThickness/2, borderThickness/2, textWidth + borderThickness, fontsize * 1.4 + borderThickness, 6);
-   // 1.4 is extra height factor for text below baseline: g,j,p,q.
-   
-   // text color
-   context.fillStyle = "rgba(0, 0, 0, 1.0)";
-
-   context.fillText( message, borderThickness, fontsize + borderThickness);
-   
-   // canvas contents will be used for a texture
-   var texture = new THREE.Texture(canvas) 
-   texture.needsUpdate = true;
-
-   var spriteMaterial = new THREE.SpriteMaterial( 
-      { map: texture, useScreenCoordinates: false, alignment: spriteAlignment } );
-   var sprite = new THREE.Sprite( spriteMaterial );
-   sprite.scale.set(100,50,1.0);
-   return sprite; 
-}
-
-// function for drawing rounded rectangles
-function roundRect(ctx, x, y, w, h, r) 
-{
-    ctx.beginPath();
-    ctx.moveTo(x+r, y);
-    ctx.lineTo(x+w-r, y);
-    ctx.quadraticCurveTo(x+w, y, x+w, y+r);
-    ctx.lineTo(x+w, y+h-r);
-    ctx.quadraticCurveTo(x+w, y+h, x+w-r, y+h);
-    ctx.lineTo(x+r, y+h);
-    ctx.quadraticCurveTo(x, y+h, x, y+h-r);
-    ctx.lineTo(x, y+r);
-    ctx.quadraticCurveTo(x, y, x+r, y);
-    ctx.closePath();
-    ctx.fill();
-   ctx.stroke();   
-}
