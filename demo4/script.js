b04e124d61ebfd8a3d1aff9b5ced3ca14317531c
adrianalonsoba <adrian.alonsoba@gmail.com>
2015-07-14 11:06:01 +0200

    añadida transparencia a demo4, hay un bug: cuando se selecciona fuera de las figuras estas pierden el color

--- demo4/script.js
+++ demo4/script.js
@@ -10,6 +10,8 @@ var json_data;
 //var keyboard = new THREEx.KeyboardState();
 var clock = new THREE.Clock();
 
+var projector, mouse = { x: 0, y: 0 }, INTERSECTED;
+
 
 // initialization
   //getJSON call, draw cubes with data
@@ -145,12 +147,12 @@ function init () {
 
   var charGeometry = new THREE.ExtrudeGeometry( charShape, extrusionSettings );
   
-  var materialFront = new THREE.MeshLambertMaterial( { color: 0x0000ff } );
-  var materialSide = new THREE.MeshLambertMaterial( { color: 0x0000ff } );
-  var materialArray = [ materialFront, materialSide ];
-  var extrudeChartMaterial = new THREE.MeshFaceMaterial(materialArray);
+  //var materialFront = new THREE.MeshLambertMaterial( { color: 0x0000ff } );
+  var materialSide = new THREE.MeshLambertMaterial( { color: 0x0000ff,transparent:true,opacity:0.5 } );
+  //var materialArray = [ materialFront, materialSide ];
+ // var extrudeChartMaterial = new THREE.MeshFaceMaterial(materialArray);
   
-  var extrudeChart = new THREE.Mesh( charGeometry, extrudeChartMaterial );
+  var extrudeChart = new THREE.Mesh( charGeometry, materialSide );
   extrudeChart.position.set(0,0,0);
   scene.add(extrudeChart);
 
@@ -176,12 +178,12 @@ function init () {
 
   var charGeometry = new THREE.ExtrudeGeometry( charShape, extrusionSettings );
   
-  var materialFront = new THREE.MeshLambertMaterial( { color: 0xff0000 } );
-  var materialSide = new THREE.MeshLambertMaterial( { color: 0xff0000 } );
-  var materialArray = [ materialFront, materialSide ];
-  var extrudeChartMaterial = new THREE.MeshFaceMaterial(materialArray);
+  //var materialFront = new THREE.MeshLambertMaterial( { color: 0xff0000 } );
+  var materialSide = new THREE.MeshLambertMaterial( { color: 0xff0000,transparent:true,opacity:0.5} );
+  //var materialArray = [ materialFront, materialSide ];
+  //var extrudeChartMaterial = new THREE.MeshFaceMaterial(materialArray);
   
-  var extrudeChart = new THREE.Mesh( charGeometry, extrudeChartMaterial );
+  var extrudeChart = new THREE.Mesh( charGeometry,  materialSide  );
   extrudeChart.position.set(0,0,100);
   scene.add(extrudeChart);
   
@@ -192,8 +194,26 @@ function init () {
  // scene.add( rectMesh );
 
 
+  // initialize object to perform world/screen calculations
+  projector = new THREE.Projector();
+  
+  // when the mouse moves, call the given function
+  document.addEventListener( 'mousemove', onDocumentMouseMove, false );
+
 
 }
+
+function onDocumentMouseMove( event ) 
+{
+  // the following line would stop any other event handler from firing
+  // (such as the mouse's TrackballControls)
+  // event.preventDefault();
+  
+  // update the mouse variable
+  mouse.x = ( event.clientX / window.innerWidth ) * 2 - 1;
+  mouse.y = - ( event.clientY / window.innerHeight ) * 2 + 1;
+}
+
 
 function animate() 
 {
@@ -210,8 +230,53 @@ function render()
 
 function update()
 {
-   // delta = change in time since last call (in seconds)
-   var delta = clock.getDelta(); 
-   controls.update();
+  // find intersections
+
+  // create a Ray with origin at the mouse position
+  //   and direction into the scene (camera direction)
+  var vector = new THREE.Vector3( mouse.x, mouse.y, 1 );
+  projector.unprojectVector( vector, camera );
+  var ray = new THREE.Raycaster( camera.position, vector.sub( camera.position ).normalize() );
+
+  // create an array containing all objects in the scene with which the ray intersects
+  var intersects = ray.intersectObjects( scene.children );
+
+  // INTERSECTED = the object in the scene currently closest to the camera 
+  //    and intersected by the Ray projected from the mouse position  
+  
+  // if there is one (or more) intersections
+  if ( intersects.length > 0 )
+  {
+    // if the closest object intersected is not the currently stored intersection object
+    if ( intersects[ 0 ].object != INTERSECTED ) 
+    {
+        // restore previous intersection object (if it exists) to its original color
+      if ( INTERSECTED ) 
+        //INTERSECTED.material.color.setHex( INTERSECTED.currentHex );
+        INTERSECTED.material.opacity= 0.5;
+
+      // store reference to closest object as current intersection object
+      INTERSECTED = intersects[ 0 ].object;
+      // store color of closest object (for later restoration)
+      //INTERSECTED.currentHex = INTERSECTED.material.color.getHex();
+
+      // set a new color for closest object
+     // INTERSECTED.material.color.setHex( 0xffff00 );
+      INTERSECTED.material.opacity=1;
+
+    }
+  } 
+  else // there are no intersections
+  {
+    // restore previous intersection object (if it exists) to its original color
+    if ( INTERSECTED ) 
+      INTERSECTED.material.color.setHex( INTERSECTED.currentHex );
+    // remove previous intersection object reference
+    //     by setting current intersection object to "nothing"
+    INTERSECTED = null;
+  }
+  
+  controls.update();
+  //stats.update();
 }
 
