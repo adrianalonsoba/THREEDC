e01f68b970ab7773644adef6805095ba031bc86b
adrianalonsoba <adrian.alonsoba@gmail.com>
2015-09-02 10:28:27 +0200

    tarta completada con huecos

--- script.js
+++ script.js
@@ -7,34 +7,10 @@
 var container, scene, camera, renderer, controls, stats;
 //JSON data saved here
 var json_data;
-//var keyboard = new THREEx.KeyboardState();
-var clock = new THREE.Clock();
-
-// custom global variables
-var projector, mouse = { x: 0, y: 0 }, INTERSECTED;
-var sprite1;
-//graphical user interface
-var gui;
-var parameters;
-
-var pies = [];
-
-  var  extrudeOpts = {
-    size: 30, height: 4, curveSegments: 3,
-    bevelThickness: 1, bevelSize: 2, bevelEnabled: false,
-    material: 0, extrudeMaterial: 1
-  };
-
-var canvas1, context1, texture1;
-var current_mini_chart1;
-var current_mini_chart2;
-//displacement of fixed minichart
-var dis=60;
-var fixed_minicharts=[];
 
 // initialization
   //getJSON call, draw cubes with data
-   $.getJSON("jsons/scm-evolutionary.json", function(data) {
+   $.getJSON("jsons/example.json", function(data) {
       json_data=data;
       init();
       // animation loop / game loop
@@ -83,7 +59,7 @@ function init () {
    // attach renderer to the container div
    container.appendChild( renderer.domElement );
 
-    ////////////
+       ////////////
   // EVENTS //
   ////////////
 
@@ -104,20 +80,10 @@ function init () {
    ///////////
    // LIGHT //
    ///////////
-   var light = new THREE.PointLight(0xffffff,1, 100000);
+   var light = new THREE.PointLight(0xffffff,1);
    light.position.set(250,250,250);
    scene.add(light);
-
-   var light = new THREE.PointLight(0xffffff,1, 100000);
-   light.position.set(250,-250,250);
-   scene.add(light);
-
-   var light = new THREE.PointLight(0xffffff,1, 100000);
-   light.position.set(250,250,-250);
-   scene.add(light);
-
-
-   var ambientLight = new THREE.AmbientLight(0x111111);
+      var ambientLight = new THREE.AmbientLight(0x111111);
    // scene.add(ambientLight);
 
    // create a set of coordinate axes to help orient user
@@ -126,7 +92,7 @@ function init () {
    scene.add(axes);
 
    // note: 4x4 checkboard pattern scaled so that each square is 25 by 25 pixels.
-   var floorTexture = new THREE.ImageUtils.loadTexture( 'images/checkerboard.jpg' );
+   var floorTexture = new THREE.ImageUtils.loadTexture( '../images/checkerboard.jpg' );
    floorTexture.wrapS = floorTexture.wrapT = THREE.RepeatWrapping; 
    floorTexture.repeat.set( 10, 10 );
    // DoubleSide: render texture on both sides of mesh
@@ -141,8 +107,8 @@ function init () {
    // BackSide: render faces from inside of the cube, instead of from outside (default).
    var skyBoxMaterial = new THREE.MeshBasicMaterial( { color: 0x9999ff, side: THREE.BackSide } );
    var skyBox = new THREE.Mesh( skyBoxGeometry, skyBoxMaterial );
-  // scene.add(skyBox);
-   scene.fog = new THREE.FogExp2( 0x9999ff, 0.00025 );
+   scene.add(skyBox);
+  // scene.fog = new THREE.FogExp2( 0x9999ff, 0.00025 );
 
    //////////////
    // GEOMETRY //
@@ -150,16 +116,18 @@ function init () {
 
    // most objects displayed are a "mesh":
    //  a collection of points ("geometry") and
-   //  a set of surface args ("material")
+   //  a set of surface parameters ("material")
 
-
-  //*** Adding pies
-
+	var  extrudeOpts = {
+		size: 30, height: 4, curveSegments: 3,
+		bevelThickness: 1, bevelSize: 2, bevelEnabled: false,
+		material: 0, extrudeMaterial: 1
+	};
   	var val=3;
-  	var valTotal=30;
+  	var valTotal=50;
   	var pieRadius=50;
   	var angPrev=0;
-  	var angToMove=90;
+  	var angToMove;
 
     var material = new THREE.MeshPhongMaterial( {color: 0xff0000} );
 
@@ -171,7 +139,7 @@ function init () {
     shape.arc(0,0,pieRadius,angPrev,
               angPrev+angToMove,false);
     shape.lineTo(0,0);
-    nextAng = this.angPrev + angToMove;
+    nextAng = angPrev + angToMove;
 
     var geometry = new THREE.ExtrudeGeometry( shape, extrudeOpts );
 
@@ -185,101 +153,88 @@ function init () {
 
     scene.add(pieobj );
 
-	// initialize object to perform world/screen calculations
-	projector = new THREE.Projector();
+    angPrev=nextAng;
+    val=20;
 
-	// when the mouse moves, call the given function
-	document.addEventListener( 'mousemove', onDocumentMouseMove, false );
-	document.addEventListener( 'mousedown', onDocumentMouseDown, false );
-	/////// draw text on canvas /////////
-
-	// create a canvas element
-	canvas1 = document.createElement('canvas');
-	context1 = canvas1.getContext('2d');
-	context1.font = "Bold 20px Arial";
-	context1.fillStyle = "rgba(0,0,0,0.95)";
-    context1.fillText('Hello, world!', 0, 20);
-    
-	// canvas contents will be used for a texture
-	texture1 = new THREE.Texture(canvas1) 
-	texture1.needsUpdate = true;
-	
-	////////////////////////////////////////
-
-	
-	var spriteMaterial = new THREE.SpriteMaterial( { map: texture1, useScreenCoordinates: true, alignment: THREE.SpriteAlignment.topLeft } );
-	
-	sprite1 = new THREE.Sprite( spriteMaterial );
-	sprite1.scale.set(200,100,1.0);
-	sprite1.position.set( 50, 50, 0 );
-	scene.add( sprite1 );	
-
-	//////////////////////////////////////////
-
-	//GUI//
-	var gui = new dat.GUI();
-	
-	parameters = 
-	{
-		reset: function() { resetFixedcharts() }
-	};
-	
-
-	gui.add( parameters, 'reset' ).name("Reset fixed charts");
-	
-	gui.close();
-	//////
-
-}
-
-function resetFixedcharts (argument) {
-	dis=60;
-	for (var i = 0; i < fixed_minicharts.length; i++) {
-		scene.remove(fixed_minicharts[i]);
-	};
-}
-
-function get_random_color() {
-  function c() {
-    return Math.floor(Math.random()*256).toString(16)
-  }
-  return "#"+c()+c()+c();
-}
-
-function onDocumentMouseMove( event ) 
-{
-	// the following line would stop any other event handler from firing
-	// (such as the mouse's TrackballControls)
-	// event.preventDefault();
-
-	// update sprite position
-	sprite1.position.set( event.clientX, event.clientY - 20, 0 );
-	
-	// update the mouse variable
-	mouse.x = ( event.clientX / window.innerWidth ) * 2 - 1;
-	mouse.y = - ( event.clientY / window.innerHeight ) * 2 + 1;
-}
+    var material = new THREE.MeshPhongMaterial( {color: 0x0000ff} );
 
 
-function onDocumentMouseDown( event ) 
-{
-	// find intersections
+    // Creats the shape, based on the value and the radius
+    var shape = new THREE.Shape();
+    var angToMove = (Math.PI*2*(val/valTotal));
+    shape.moveTo(0,0);
+    shape.arc(0,0,pieRadius,angPrev,
+              angPrev+angToMove,false);
+    shape.lineTo(0,0);
+    nextAng = angPrev + angToMove;
 
-	// create a Ray with origin at the mouse position
-	//   and direction into the scene (camera direction)
-	var vector = new THREE.Vector3( mouse.x, mouse.y, 1 );
-	projector.unprojectVector( vector, camera );
-	var ray = new THREE.Raycaster( camera.position, vector.sub( camera.position ).normalize() );
+    var geometry = new THREE.ExtrudeGeometry( shape, extrudeOpts );
 
-	// create an array containing all objects in the scene with which the ray intersects
-	var intersects = ray.intersectObjects( scene.children );
-	
-	// if there is one (or more) intersections
-	if ( intersects.length > 0 )
-	{
-		console.log(intersects[0].object.name);
-		create_fixed_chart(intersects[0].object);
-	}
+    var pieobj = new THREE.Mesh( geometry, material );
+    pieobj.rotation.set(90,0,0);
+                                          
+    // Creating the 3D object, positioning it and adding it to the scene
+    pieobj = new THREE.Mesh( geometry, material );
+    pieobj.rotation.set(Math.PI/2,0,0);
+    // Adds shadows if selected as an option
+
+    scene.add(pieobj );
+  
+    angPrev=nextAng;
+    val=7;
+
+    var material = new THREE.MeshPhongMaterial( {color: 0xff00ff} );
+
+
+    // Creats the shape, based on the value and the radius
+    var shape = new THREE.Shape();
+    var angToMove = (Math.PI*2*(val/valTotal));
+    shape.moveTo(0,0);
+    shape.arc(0,0,pieRadius,angPrev,
+              angPrev+angToMove,false);
+    shape.lineTo(0,0);
+    nextAng = angPrev + angToMove;
+
+    var geometry = new THREE.ExtrudeGeometry( shape, extrudeOpts );
+
+    var pieobj = new THREE.Mesh( geometry, material );
+    pieobj.rotation.set(90,0,0);
+                                          
+    // Creating the 3D object, positioning it and adding it to the scene
+    pieobj = new THREE.Mesh( geometry, material );
+    pieobj.rotation.set(Math.PI/2,0,0);
+    // Adds shadows if selected as an option
+
+    scene.add(pieobj );
+
+
+    angPrev=nextAng;
+    val=20;
+
+    var material = new THREE.MeshPhongMaterial( {color: 0xffffff} );
+
+
+    // Creats the shape, based on the value and the radius
+    var shape = new THREE.Shape();
+    var angToMove = (Math.PI*2*(val/valTotal));
+    shape.moveTo(0,0);
+    shape.arc(0,0,pieRadius,angPrev,
+              angPrev+angToMove,false);
+    shape.lineTo(0,0);
+    nextAng = angPrev + angToMove;
+
+    var geometry = new THREE.ExtrudeGeometry( shape, extrudeOpts );
+
+    var pieobj = new THREE.Mesh( geometry, material );
+    pieobj.rotation.set(90,0,0);
+                                          
+    // Creating the 3D object, positioning it and adding it to the scene
+    pieobj = new THREE.Mesh( geometry, material );
+    pieobj.rotation.set(Math.PI/2,0,0);
+    // Adds shadows if selected as an option
+
+    scene.add(pieobj );
+
 }
 
 function animate() 
@@ -289,124 +244,12 @@ function animate()
    update();
 }
 
-
 function render() 
 {  
    renderer.render( scene, camera );
 }
 
-function create_mini_chart (args) {
-	if(args.info){
-		scene.remove(current_mini_chart1);
-		scene.remove(current_mini_chart2);
-
-		var geometry = new THREE.CubeGeometry( 10,args.info.commits/10 , 10);
-		var material = new THREE.MeshLambertMaterial( {map:texture} );
-		var cube = new THREE.Mesh(geometry, material);
-		cube.position.set(0, args.info.commits/10/2, 30);
-		cube.name = "Commits:"+args.info.commits+" - "+args.info.date;
-		scene.add(cube);
-		current_mini_chart1=cube;
-
-		geometry = new THREE.CubeGeometry( 10,args.info.authors/10 , 10);
-		material = new THREE.MeshLambertMaterial( {color: "#ff0000"} );
-		cube = new THREE.Mesh(geometry, material);
-		cube.position.set(20, args.info.authors/10/2, 30);
-		cube.name = "Autors:"+args.info.authors+" - "+args.info.date;;
-		scene.add(cube);
-		current_mini_chart2=cube;
-	}
-}
-
-function create_fixed_chart (args) {
-	var geometry = new THREE.CubeGeometry( 10,args.info.commits/10 , 10);
-	var material = new THREE.MeshLambertMaterial( {map:texture} );
-	var cube = new THREE.Mesh(geometry, material);
-	cube.position.set(0, args.info.commits/10/2, dis);
-	cube.name = "Commits:"+args.info.commits+" - "+args.info.date;
-	scene.add(cube);
-
-	fixed_minicharts.push(cube);
-
-	geometry = new THREE.CubeGeometry( 10,args.info.authors/10 , 10);
-	material = new THREE.MeshLambertMaterial( {color: "#ff0000"} );
-	cube = new THREE.Mesh(geometry, material);
-	cube.position.set(20, args.info.authors/10/2, dis);
-	cube.name = "Autors:"+args.info.authors+" - "+args.info.date;;
-	scene.add(cube);
-
-	fixed_minicharts.push(cube);
-	dis+=30;
-}
-
-
 function update()
 {
-	
-	// create a Ray with origin at the mouse position
-	//   and direction into the scene (camera direction)
-	var vector = new THREE.Vector3( mouse.x, mouse.y, 1 );
-	projector.unprojectVector( vector, camera );
-	var ray = new THREE.Raycaster( camera.position, vector.sub( camera.position ).normalize() );
-
-	// create an array containing all objects in the scene with which the ray intersects
-	var intersects = ray.intersectObjects( scene.children );
-
-	// INTERSECTED = the object in the scene currently closest to the camera 
-	//		and intersected by the Ray projected from the mouse position 	
-	
-	// if there is one (or more) intersections
-	if ( intersects.length > 0 )
-	{
-		// if the closest object intersected is not the currently stored intersection object
-		if ( intersects[ 0 ].object != INTERSECTED ) 
-		{
-		    // restore previous intersection object (if it exists) to its original color
-			if ( INTERSECTED ) 
-				INTERSECTED.material.color.setHex( INTERSECTED.currentHex );
-			// store reference to closest object as current intersection object
-			INTERSECTED = intersects[ 0 ].object;
-			// store color of closest object (for later restoration)
-			INTERSECTED.currentHex = INTERSECTED.material.color.getHex();
-			// set a new color for closest object
-			INTERSECTED.material.color.setHex( 0xffff00 );
-			
-			// update text, if it has a "name" field.
-			if ( intersects[ 0 ].object.name )
-			{
-			    context1.clearRect(0,0,640,480);
-				var message = intersects[ 0 ].object.name;
-				var metrics = context1.measureText(message);
-				var width = metrics.width;
-				context1.fillStyle = "rgba(0,0,0,0.95)"; // black border
-				context1.fillRect( 0,0, width+8,20+8);
-				context1.fillStyle = "rgba(255,255,255,0.95)"; // white filler
-				context1.fillRect( 2,2, width+4,20+4 );
-				context1.fillStyle = "rgba(0,0,0,1)"; // text color
-				context1.fillText( message, 4,20 );
-				texture1.needsUpdate = true;
-				create_mini_chart(intersects[ 0 ].object);
-			}
-			else
-			{
-				context1.clearRect(0,0,300,300);
-				texture1.needsUpdate = true;
-			}
-		}
-	} 
-	else // there are no intersections
-	{
-		// restore previous intersection object (if it exists) to its original color
-		if ( INTERSECTED ) 
-			INTERSECTED.material.color.setHex( INTERSECTED.currentHex );
-		// remove previous intersection object reference
-		//     by setting current intersection object to "nothing"
-		INTERSECTED = null;
-		context1.clearRect(0,0,300,300);
-		texture1.needsUpdate = true;
-	}
-	controls.update();
+   controls.update();
 }
-
-
-
