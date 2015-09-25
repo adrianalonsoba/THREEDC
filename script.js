d464130ee8d3189cd53de708009adee0ffc80538
adrianalonsoba <adrian.alonsoba@gmail.com>
2015-09-25 11:51:26 +0200

    añadida forma alternativa en pruebas  de manejar eventos sin raycasting

--- script.js
+++ script.js
@@ -27,6 +27,8 @@ var fixed_minicharts=[];
 
 var  extrudeOpts = {curveSegments:300, amount: 8, bevelEnabled: false, bevelSegments: 2, steps: 2, bevelSize: 1, bevelThickness: 1 };
 
+var domEvents;
+
 
 //CROSSFILTER VARS
 
@@ -146,8 +148,8 @@ function init () {
    // BackSide: render faces from inside of the cube, instead of from outside (default).
    var skyBoxMaterial = new THREE.MeshBasicMaterial( { color: 0x9999ff, side: THREE.BackSide } );
    var skyBox = new THREE.Mesh( skyBoxGeometry, skyBoxMaterial );
-  // scene.add(skyBox);
-   scene.fog = new THREE.FogExp2( 0x9999ff, 0.00025 );
+   scene.add(skyBox);
+   //scene.fog = new THREE.FogExp2( 0x9999ff, 0.00025 );
 
    //////////////
    // CUSTOM //
@@ -191,22 +193,20 @@ function init () {
    dimByOrg= cf.dimension(function(p) {return p.org;});
 
    groupByOrg= dimByOrg.group();
+   
+   domEvents  = new THREEx.DomEvents(camera, renderer.domElement)
 
-
-   //COMO ordeno por fecha?¿?¿?¿?¿
 
      //by month
   drawBars();
 
-    //by org
-  drawPie();
 
   // initialize object to perform world/screen calculations
-  projector = new THREE.Projector();
+  //projector = new THREE.Projector();
 
   // when the mouse moves, call the given function
-  document.addEventListener( 'mousemove', onDocumentMouseMove, false );
-  document.addEventListener( 'mousedown', onDocumentMouseDown, false );
+  //document.addEventListener( 'mousemove', onDocumentMouseMove, false );
+  //document.addEventListener( 'mousedown', onDocumentMouseDown, false );
   /////// draw text on canvas /////////
 
   // create a canvas element
@@ -220,15 +220,16 @@ function init () {
   texture1 = new THREE.Texture(canvas1)
   texture1.needsUpdate = true;
 
-  ////////////////////////////////////////
 
-
-  var spriteMaterial = new THREE.SpriteMaterial( { map: texture1, useScreenCoordinates: true, alignment: THREE.SpriteAlignment.topLeft } );
-
+  var spriteMaterial = new THREE.SpriteMaterial( { map: texture1, useScreenCoordinates: true } );
+  
   sprite1 = new THREE.Sprite( spriteMaterial );
   sprite1.scale.set(200,100,1.0);
   sprite1.position.set( 50, 50, 0 );
-  scene.add( sprite1 );
+  scene.add( sprite1 ); 
+
+  ////////////////////////////////////////
+
 
   //////////////////////////////////////////
 
@@ -304,6 +305,26 @@ function drawBars () {
       };
       scene_objects2.push(cube);
       scene.add(cube);
+
+      domEvents.addEventListener(cube,'click',function(event) {
+        alert(cube.name);
+      },false);
+
+      domEvents.addEventListener(cube,'mouseover',function(event) {
+        cube.material.color.setHex(0xffff00);
+        context1.clearRect(0,0,640,480);
+        var message = cube.name;
+        var metrics = context1.measureText(message);
+        var width = metrics.width;
+        context1.fillStyle = "rgba(0,0,0,0.95)"; // black border
+        context1.fillRect( 0,0, width+8,20+8);
+        context1.fillStyle = "rgba(255,255,255,0.95)"; // white filler
+        context1.fillRect( 2,2, width+4,20+4 );
+        context1.fillStyle = "rgba(0,0,0,1)"; // text color
+        context1.fillText( message, 4,20 );
+        sprite1.position.set( cube.position.x,cube.position.y, cube.position.z );
+        texture1.needsUpdate = true;
+      },false);
       x+=1;
    });
 }
@@ -436,67 +457,5 @@ function render()
 
 function update()
 {
-
-  // create a Ray with origin at the mouse position
-  //   and direction into the scene (camera direction)
-  var vector = new THREE.Vector3( mouse.x, mouse.y, 1 );
-  projector.unprojectVector( vector, camera );
-  var ray = new THREE.Raycaster( camera.position, vector.sub( camera.position ).normalize() );
-
-  // create an array containing all objects in the scene with which the ray intersects
-  var intersects = ray.intersectObjects( scene.children );
-
-  // INTERSECTED = the object in the scene currently closest to the camera
-  //    and intersected by the Ray projected from the mouse position
-
-  // if there is one (or more) intersections
-  if ( intersects.length > 0 )
-  {
-    // if the closest object intersected is not the currently stored intersection object
-    if ( intersects[ 0 ].object != INTERSECTED )
-    {
-        // restore previous intersection object (if it exists) to its original color
-      if ( INTERSECTED )
-        INTERSECTED.material.color.setHex( INTERSECTED.currentHex );
-      // store reference to closest object as current intersection object
-      INTERSECTED = intersects[ 0 ].object;
-      // store color of closest object (for later restoration)
-      INTERSECTED.currentHex = INTERSECTED.material.color.getHex();
-      // set a new color for closest object
-      INTERSECTED.material.color.setHex( 0xffff00 );
-
-      // update text, if it has a "name" field.
-      if ( intersects[ 0 ].object.name )
-      {
-          context1.clearRect(0,0,640,480);
-        var message = intersects[ 0 ].object.name;
-        var metrics = context1.measureText(message);
-        var width = metrics.width;
-        context1.fillStyle = "rgba(0,0,0,0.95)"; // black border
-        context1.fillRect( 0,0, width+8,20+8);
-        context1.fillStyle = "rgba(255,255,255,0.95)"; // white filler
-        context1.fillRect( 2,2, width+4,20+4 );
-        context1.fillStyle = "rgba(0,0,0,1)"; // text color
-        context1.fillText( message, 4,20 );
-        texture1.needsUpdate = true;
-      }
-      else
-      {
-        context1.clearRect(0,0,300,300);
-        texture1.needsUpdate = true;
-      }
-    }
-  }
-  else // there are no intersections
-  {
-    // restore previous intersection object (if it exists) to its original color
-    if ( INTERSECTED )
-      INTERSECTED.material.color.setHex( INTERSECTED.currentHex );
-    // remove previous intersection object reference
-    //     by setting current intersection object to "nothing"
-    INTERSECTED = null;
-    context1.clearRect(0,0,300,300);
-    texture1.needsUpdate = true;
-  }
   controls.update();
 }
