c077a785176219992eafca8b78ecbb481df7467f
adrianalonsoba <adrian.alonsoba@gmail.com>
2015-09-29 12:22:47 +0200

    añadida nueva interaccion con la nueva biblioteca de eventos, falta corregir algunso aspectos

--- script.js
+++ script.js
@@ -33,12 +33,11 @@ var domEvents;
 //CROSSFILTER VARS
 
 
-   var cf;
+ var cf;
 
-   var dimByMonth;
+ var dimByMonth;
 
-   var groupByMonth;
-
+ var groupByMonth;
 
   var dimByOrg;
 
@@ -48,9 +47,8 @@ var domEvents;
 
   var scene_objects2=[];
 
-
 // initialization
-  //getJSON call, draw cubes with data
+  //getJSON call, draw meshes with data
    $.getJSON("jsons/scm-commits.json", function(data) {
       json_data=data;
       init();
@@ -104,6 +102,10 @@ function init () {
   // EVENTS //
   ////////////
 
+  //with this, we can use standard dom events without raycasting
+  domEvents  = new THREEx.DomEvents(camera, renderer.domElement)
+
+
   // automatically resize renderer
   THREEx.WindowResize(renderer, camera);
     // toggle full-screen on given key press
@@ -124,7 +126,7 @@ function init () {
    var light = new THREE.PointLight(0xffffff,1);
    light.position.set(250,250,250);
    scene.add(light);
-      var ambientLight = new THREE.AmbientLight(0x111111);
+   var ambientLight = new THREE.AmbientLight(0x111111);
    // scene.add(ambientLight);
 
    // create a set of coordinate axes to help orient user
@@ -149,6 +151,13 @@ function init () {
    var skyBoxMaterial = new THREE.MeshBasicMaterial( { color: 0x9999ff, side: THREE.BackSide } );
    var skyBox = new THREE.Mesh( skyBoxGeometry, skyBoxMaterial );
    scene.add(skyBox);
+
+  //clear texture of infobox
+  domEvents.addEventListener(skyBox,'mouseover',function(event) {
+      context1.clearRect(0,0,300,300);
+      texture1.needsUpdate = true;
+  },false);
+
    //scene.fog = new THREE.FogExp2( 0x9999ff, 0.00025 );
 
    //////////////
@@ -177,7 +186,6 @@ function init () {
       parsed_data.push(record);
     });
 
-
    cf=crossfilter(parsed_data);
 
    console.log(parsed_data);
@@ -193,12 +201,13 @@ function init () {
    dimByOrg= cf.dimension(function(p) {return p.org;});
 
    groupByOrg= dimByOrg.group();
-   
-   domEvents  = new THREEx.DomEvents(camera, renderer.domElement)
-
 
      //by month
   drawBars();
+
+  //by org
+
+  drawPie();
 
 
   // initialize object to perform world/screen calculations
@@ -214,17 +223,17 @@ function init () {
   context1 = canvas1.getContext('2d');
   context1.font = "Bold 20px Arial";
   context1.fillStyle = "rgba(0,0,0,0.95)";
-    context1.fillText('Hello, world!', 0, 20);
+  context1.fillText('', 0, 20);
 
   // canvas contents will be used for a texture
   texture1 = new THREE.Texture(canvas1)
   texture1.needsUpdate = true;
 
 
-  var spriteMaterial = new THREE.SpriteMaterial( { map: texture1, useScreenCoordinates: true } );
+  var spriteMaterial = new THREE.SpriteMaterial( { map: texture1 } );
   
   sprite1 = new THREE.Sprite( spriteMaterial );
-  sprite1.scale.set(200,100,1.0);
+  sprite1.scale.set(100,50,1.0);
   sprite1.position.set( 50, 50, 0 );
   scene.add( sprite1 ); 
 
@@ -258,7 +267,9 @@ function drawPie () {
 
    groupByOrg.top(Infinity).forEach(function(p,i) {
 
-      var material = new THREE.MeshPhongMaterial( {color: get_random_color()} );
+      var hex_color=get_random_color();
+      var origin_color='#'+hex_color;
+      var material = new THREE.MeshPhongMaterial( {color:origin_color } );
       // Creats the shape, based on the value and the radius
       var shape = new THREE.Shape();
       var angToMove = (Math.PI*2*(p.value/valTotal));
@@ -270,6 +281,7 @@ function drawPie () {
 
       var geometry = new THREE.ExtrudeGeometry( shape, extrudeOpts );
       var pieobj = new THREE.Mesh( geometry, material );
+      pieobj.origin_color=hex_color;
       pieobj.rotation.set(0,0,0);
       pieobj.position.set(-75,0,0);
       pieobj.name = "Commits:"+p.value+" Org:"+p.key;
@@ -281,8 +293,21 @@ function drawPie () {
 
       scene_objects1.push(pieobj);
       angPrev=nextAng;
-   });
 
+      domEvents.addEventListener(pieobj,'click',function(event) {
+        redraw2(pieobj.info.org);
+      },false);
+
+      domEvents.addEventListener(pieobj,'mouseover',function(event) {
+        changeMeshColor(pieobj);
+        showInfo(pieobj);
+      },false);
+
+      domEvents.addEventListener(pieobj,'mouseout',function(event) {
+        pieobj.material.color.setHex('0x'+pieobj.origin_color);
+      },false);
+
+   });
 }
 
 function drawBars () {
@@ -295,8 +320,10 @@ function drawBars () {
       //commit values are normalized to optimal visualization(/10)
       var geometry = new THREE.CubeGeometry( 1, p.value/10, 10);
       y=p.value/10/2;
-      var material = new THREE.MeshLambertMaterial( {color: "#0000ff"} );
+      var origin_color=0x0000ff;
+      var material = new THREE.MeshLambertMaterial( {color: origin_color} );
       var cube = new THREE.Mesh(geometry, material);
+      cube.origin_color=origin_color;
       cube.position.set(x, y, z);
       cube.name = "Commits:"+p.value+" "+p.key;
       cube.info={
@@ -305,36 +332,49 @@ function drawBars () {
       };
       scene_objects2.push(cube);
       scene.add(cube);
+      x+=1;
 
       domEvents.addEventListener(cube,'click',function(event) {
-        alert(cube.name);
+        redraw1(cube.info.month);
       },false);
 
       domEvents.addEventListener(cube,'mouseover',function(event) {
-        cube.material.color.setHex(0xffff00);
-        context1.clearRect(0,0,640,480);
-        var message = cube.name;
-        var metrics = context1.measureText(message);
-        var width = metrics.width;
-        context1.fillStyle = "rgba(0,0,0,0.95)"; // black border
-        context1.fillRect( 0,0, width+8,20+8);
-        context1.fillStyle = "rgba(255,255,255,0.95)"; // white filler
-        context1.fillRect( 2,2, width+4,20+4 );
-        context1.fillStyle = "rgba(0,0,0,1)"; // text color
-        context1.fillText( message, 4,20 );
-        sprite1.position.set( cube.position.x,cube.position.y, cube.position.z );
-        texture1.needsUpdate = true;
+        changeMeshColor(cube);
+        showInfo(cube);
       },false);
-      x+=1;
+
+      domEvents.addEventListener(cube,'mouseout',function(event) {
+        cube.material.color.setHex(cube.origin_color);
+      },false);
    });
 }
 
+//shows message in a sprite
+function showInfo (mesh) {
+  context1.clearRect(0,0,640,480);
+  var message = mesh.name;
+  var metrics = context1.measureText(message);
+  var width = metrics.width;
+  context1.fillStyle = "rgba(0,0,0,0.95)"; // black border
+  context1.fillRect( 0,0, width+8,20+8);
+  context1.fillStyle = "rgba(255,255,255,0.95)"; // white filler
+  context1.fillRect( 2,2, width+4,20+4 );
+  context1.fillStyle = "rgba(0,0,0,1)"; // text color
+  context1.fillText( message, 4,20 );
+  sprite1.position.set( mesh.position.x,mesh.position.y, mesh.position.z );
+  texture1.needsUpdate = true;
+}
+
+//changes mesh color when selected
+function changeMeshColor (mesh) {
+  mesh.material.color.setHex(0xffff00);
+}
 
 function get_random_color() {
   function c() {
     return Math.floor(Math.random()*256).toString(16)
   }
-  return "#"+c()+c()+c();
+  return c()+c()+c();
 }
 
 function clearFilters () {
@@ -349,51 +389,6 @@ function clearFilters () {
   dimByOrg.filterAll();
   drawBars();
   drawPie();
-
-
-}
-
-function onDocumentMouseMove( event )
-{
-  // the following line would stop any other event handler from firing
-  // (such as the mouse's TrackballControls)
-  // event.preventDefault();
-
-  // update sprite position
-  sprite1.position.set( event.clientX, event.clientY - 20, 0 );
-
-  // update the mouse variable
-  mouse.x = ( event.clientX / window.innerWidth ) * 2 - 1;
-  mouse.y = - ( event.clientY / window.innerHeight ) * 2 + 1;
-}
-
-
-function onDocumentMouseDown( event )
-{
-  // find intersections
-
-  // create a Ray with origin at the mouse position
-  //   and direction into the scene (camera direction)
-  var vector = new THREE.Vector3( mouse.x, mouse.y, 1 );
-  projector.unprojectVector( vector, camera );
-  var ray = new THREE.Raycaster( camera.position, vector.sub( camera.position ).normalize() );
-
-  // create an array containing all objects in the scene with which the ray intersects
-  var intersects = ray.intersectObjects( scene.children );
-
-  // if there is one (or more) intersections
-  if ( intersects.length > 0 )
-  {
-    //create_fixed_chart(intersects[0].object);
-    if(intersects[0].object.info.month){
-      redraw1(intersects[0].object.info.month);
-    }
-
-    if(intersects[0].object.info.org||intersects[0].object.info.org===0){
-      redraw2(intersects[0].object.info.org);
-    }
-
-  }
 }
 
 function redraw1 (argument) {
@@ -404,8 +399,6 @@ function redraw1 (argument) {
 
   dimByMonth.filter(argument);
 
-  //POR QUE NO FUNCIONA SI LE APLICO EL FILTRO A DIMBYORG??????
-
   console.log("Numero commits en ese mes"+dimByMonth.top(Infinity).length);
 
   for (var i = 0; i < scene_objects1.length; i++) {
@@ -413,8 +406,6 @@ function redraw1 (argument) {
   };
 
   drawPie();
-
-  //HABRIA QUE LIMPIAR EL FILTRO DESPUES DE REPINTAR?¿?¿?¿
 
 }
 
@@ -424,10 +415,7 @@ function redraw2 (argument) {
 
   dimByOrg.filterAll();
 
-
   dimByOrg.filter(argument);
-
-  //POR QUE NO FUNCIONA SI LE APLICO EL FILTRO A DIMBYORG??????
 
   console.log("Numero commits en ese mes en esa org"+dimByMonth.top(Infinity).length);
 
@@ -436,11 +424,7 @@ function redraw2 (argument) {
   };
 
   drawBars();
-
-  //HABRIA QUE LIMPIAR EL FILTRO DESPUES DE REPINTAR?¿?¿?¿
-
 }
-
 
 function animate()
 {
@@ -448,7 +432,6 @@ function animate()
    render();
    update();
 }
-
 
 function render()
 {
