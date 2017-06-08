a05036b5a401126341f013d6c133b0af4eff9ffc
adrianalonsoba <adrian.alonsoba@gmail.com>
2017-06-08 19:21:57 +0200

    actualizada demo

--- mozillademo/script.js
+++ mozillademo/script.js
@@ -1,12 +1,4 @@
  
-function download(text, name, type) {
-    var a = document.createElement("a");
-    var file = new Blob([text], {type: type});
-    a.href = URL.createObjectURL(file);
-    a.download = name;
-    a.click();
-}
-
 // initialization
 //getJSON call, draw meshes with data
 $.getJSON("../jsons/webvr-git-parsed.json", function (data) {
@@ -17,9 +9,26 @@ $.getJSON("../jsons/webvr-git-parsed.json", function (data) {
 
  function init (data) {
 
-    var cf = crossfilter(data);
+    var keysObj = Object.keys(json_data[0]);
+    var parsed_data = [];
+    json_data.forEach(function (value) {
+        var record = {};
 
-    console.log(cf.size(),data);
+        keysObj.forEach(function (name) {
+            if (name == "utc_author") {
+                var date = new Date(value[name]);
+                record[name] = date;
+            } else {
+                record[name] = value[name];
+            }
+        });
+        parsed_data.push(record);
+    });
+
+    console.log('parsed',parsed_data[0]);
+    var cf = crossfilter(parsed_data);
+
+    console.log(cf.size(),data[0]);
 
     // CREATE A DASHBOARD
     var scenediv=document.getElementById( 'ThreeJS' );
@@ -157,13 +166,143 @@ $.getJSON("../jsons/webvr-git-parsed.json", function (data) {
     myDashBoard.addChart(barAuthors,{x:-200,y:0,z:20});
 
 
+
+// LINE CHART, COMMITS PER YEAR AND QUARTER
+
+    var dimbyYandQ = cf.dimension(function (d) {
+        return Number.parseInt(
+        d.utc_author.getFullYear().toString() +
+        (Math.floor(d.utc_author.getMonth() / 3) + 1).toString());
+    });
+
+    var groupbyYandQ = dimbyYandQ.group();
+
+
+    var lineMonths=THREEDC.lineChart();
+
+    lineMonths.dimension(dimbyYandQ)
+              .group(groupbyYandQ)
+              .setTitle('Commits per year and quarter')
+              .gridsOn(0xffffff)
+              .color(0x33FF39)
+              .width(400);
+
+    myDashBoard.addChart(lineMonths,{x:100,y:0,z:0});
+
+
+
+    //3D BARS CHART
+
+        var grouporgWeek = dimbyYandQ.group().reduce(function reduceAdd(p, v) {
+        var findorg = function (element) {
+            if (!element.key) return false;
+            return element.key === v.Author_org_name;
+        };
+        var elementIndex = p.findIndex(findorg);
+        if (elementIndex === -1) {
+            //init
+            p.push({ key: v.Author_org_name, value: 1 });
+        } else {
+            p[elementIndex].value = p[elementIndex].value + 1;
+        }
+        return p;
+    },
+
+    function reduceRemove(p, v) {
+        var findorg = function (element) {
+            if (!element.key) return false;
+            return element.key === v.Author_org_name;
+        };
+        var elementIndex = p.findIndex(findorg);
+        if (elementIndex !== -1) {
+            p[elementIndex].value = p[elementIndex].value - 1;
+            if (p[elementIndex].value <= 0) {
+                //remove that element.
+                p.splice(elementIndex, 1);
+            }
+        }
+
+
+        return p;
+    },
+
+    function reduceInitial() {
+        return [];
+    });
+
+    console.log( groupbyYandQ.all());
+    console.log( grouporgWeek.all());
+
+    var orgs = groupByOrg.top(Infinity);
+    var mydata = grouporgWeek.all();
+    var data1 = [];
+    var findorg = function (d) { return d.key === orgs[j].key; };
+    for (var i = 0 ; i < mydata.length; i++) {
+        for (var j = 0; j < orgs.length; j++) {
+            var found = mydata[i].value.find(findorg);
+            if (found) {
+                data1.push({ key1:found.key , key2: mydata[i].key, value: found.value });
+            } else {
+                data1.push({ key1:orgs[j].key , key2: mydata[i].key, value: 0 });
+            }
+        }
+    }
+
+    window.mybar3d = THREEDC.TDbarsChart();
+
+   // window.mybar3d.dimension(dimbyYandQ).gridsOn(0xffffff).group(grouporgWeek);
+
+    window.mybar3d.data(data1).width(250).depth(200).barSeparation(0.9).gridsOn(0xffffff).setTitle('Commits per week and org');
+
+
+   myDashBoard.addChart(window.mybar3d,{x:-100,y:20,z:100});
+
+
+    //UPDATE BUTTON
+
+    var obj = { Update:function(){ update(); }};
+
+    myDashBoard.gui.add(obj,'Update').name('Update');
+
+    function update() {
+
+        myDashBoard.removeChart(window.window.mybar3d);
+        var orgs = groupByOrg.top(Infinity);
+        var mydata = grouporgWeek.all();
+        var data1 = [];
+        var findorg = function (d) { return d.key === orgs[j].key; };
+        for (var i = 0 ; i < mydata.length; i++) {
+            for (var j = 0; j < orgs.length; j++) {
+                var found = mydata[i].value.find(findorg);
+                if (found) {
+                    data1.push({ key1:found.key , key2: mydata[i].key, value: found.value });
+                } else {
+                    data1.push({ key1:orgs[j].key , key2: mydata[i].key, value: 0 });
+                }
+            }
+        }
+     window.mybar3d = THREEDC.TDbarsChart();
+
+   // window.mybar3d.dimension(dimbyYandQ).gridsOn(0xffffff).group(grouporgWeek);
+
+    window.mybar3d.data(data1).width(250).depth(200).barSeparation(0.9).gridsOn(0xffffff).setTitle('Commits per week and org');
+
+
+   myDashBoard.addChart(window.mybar3d,{x:-100,y:20,z:100});
+
+
+
+    }
+
+
+
     //CLEAR FILTERS BUTTON
 
-
-    
     function clearFilters() {
         dimByOrg.filterAll();
         dimByAuthor.filterAll();
+        dimByTz.filterAll();
+        dimbyYandQ.filterAll();
         for (var i = 0; i < myDashBoard.charts.length; i++) {
             myDashBoard.charts[i].reBuild();
         }
